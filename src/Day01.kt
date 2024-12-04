b4742dcf2064a7b8e4e6443874481be70409f01e
Bruno Dreux <bruno.dreux@Brunos-MacBook-Pro.local>
2024-12-03 19:24:01 -0800

    Solution to day 1, part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -25,7 +25,32 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        val frequencyMap = mutableMapOf<Int, Int>()
+        val similaritiesList = mutableListOf<Int>()
+
+        for (line in input) {
+            val parts = line.split(Regex("\\s+")) // Split by one or more spaces
+            list1.add(parts[0].toInt())
+            list2.add(parts[1].toInt())
+        }
+
+        list1.sort()
+        list2.sort()
+
+        for (i in list2.indices) {
+            frequencyMap[list2[i]] = frequencyMap.getOrDefault(list2[i], 0) + 1
+        }
+
+//        print(frequencyMap)
+
+        for (i in list1.indices) {
+            //Go through the list 1 multiplying by the map
+            similaritiesList.add(list1[i] * frequencyMap.getOrDefault(list1[i], 0))
+        }
+
+        return similaritiesList.sum()
     }
 
 //    // Test if implementation meets criteria from the description, like:
@@ -34,12 +59,13 @@ fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
     part1(testInput).println()
+    part2(testInput).println()
 
     check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 
 }
