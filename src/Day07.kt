ccc1b5d17da3af6d802655beffd4487f9e69453f
Bruno Dreux <bruno.dreux@macbookpro.lan>
2024-12-16 21:48:54 -0300

    Scaffolding for day 7

---
+++ src/Day07.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("Day07_test")
+    part1(testInput).println()
+//    part2(testInput).println()
+
+    // Read the input from the `src/Day01.txt` file.
+//    val input = readInput("Day02")
+//    part1(input).println()
+//    part2(input).println()
+}
