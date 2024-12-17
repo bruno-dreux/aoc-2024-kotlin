18a15a8ca55e4e27b5f786c2c9669ac78f4ee7c7
Bruno Dreux <bruno.dreux@macbookpro.lan>
2024-12-16 21:41:17 -0300

    Day 6, part 2 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,6 +3,7 @@ fun main() {
     val occupiedSpaceChar: Char = '#'
     val startingPositionChar: Char = '^'
     val passedThroughChar: Char = 'X'
+    val newObstacleChar: Char = 'O'
 
     fun moveInDirection(currentRow: Int, currentColumn: Int, currentDirection: String, step: Int): Pair<Int, Int> {
         when (currentDirection) {
@@ -22,6 +23,14 @@ fun main() {
             "left" -> return "up"
         }
         return "wrong direction"
+    }
+
+    fun turnRightAndDetectLoop(currentTurn: List<String>, previousTurns: MutableList<List<String>>): Pair<String,MutableList<List<String>>> { //currentTurn is a list with currentRow, currentColumn, currentDirection
+        if (currentTurn in previousTurns) {
+            return Pair("loop",previousTurns)
+        }
+        previousTurns.add(currentTurn)
+        return Pair(turnRight(currentTurn[2]), previousTurns)
     }
 
     fun printLab(labMap: MutableList<MutableList<Char>>) {
@@ -73,24 +82,88 @@ fun main() {
 //            printLab(labMap)
 
         }
-
-
         return labMap.flatten().count { it == passedThroughChar }
     }
 
 
     fun part2(input: List<String>): Int {
-        return 0
+        val originalLabMap: MutableList<MutableList<Char>> = input.map { it.toMutableList() }.toMutableList()
+        var labMap = originalLabMap.map { it.toMutableList() }.toMutableList()
+        printLab(labMap)
+
+
+        var currentRow: Int = -1
+        var currentColumn: Int = -1
+        var startingRow: Int = -1
+        var startingColumn: Int = -1
+
+        var obstructionPossibilitiesCount: Int = 0
+
+        //Find the starting position
+        for (row in labMap.indices) {
+            for (column in labMap[row].indices) {
+                if (labMap[row][column] == startingPositionChar) {
+                    startingRow = row
+                    startingColumn = column
+                    break
+                }
+            }
+        }
+
+        for (row in labMap.indices) {
+            for (column in labMap[row].indices) {
+                if (labMap[row][column] == emptySpaceChar) {
+                    labMap[row][column] = newObstacleChar
+//                    printLab(labMap)
+
+                    var currentDirection: String = "up"
+
+                    labMap[startingRow][startingColumn] = passedThroughChar
+                    currentRow = startingRow
+                    currentColumn = startingColumn
+
+                    val previousTurns: MutableList<List<String>> = mutableListOf()
+
+                    while (currentRow >= 0 && currentRow < labMap.size && currentColumn >= 0 && currentColumn < labMap[0].size && currentDirection != "loop") { //In bounds of the map
+                        when (labMap[currentRow][currentColumn]) {
+                            passedThroughChar, emptySpaceChar -> {
+                                labMap[currentRow][currentColumn] = passedThroughChar
+                                val move = moveInDirection(currentRow, currentColumn, currentDirection, 1)
+                                currentRow = move.first
+                                currentColumn = move.second
+                            }
+
+                            occupiedSpaceChar, newObstacleChar -> {
+                                val move = moveInDirection(currentRow, currentColumn, currentDirection, -1)
+                                currentRow = move.first
+                                currentColumn = move.second
+                                val currentTurn: MutableList<String> = mutableListOf()
+                                currentTurn.add(currentRow.toString())
+                                currentTurn.add(currentColumn.toString())
+                                currentTurn.add(currentDirection)
+                                val executedTurn = turnRightAndDetectLoop(currentTurn, previousTurns)
+                                currentDirection = executedTurn.first
+                                previousTurns.add(currentTurn)
+                            }
+                        }
+                    }
+                    labMap = originalLabMap.map { it.toMutableList() }.toMutableList()
+                    if (currentDirection == "loop") {
+                        obstructionPossibilitiesCount++
+                        println(obstructionPossibilitiesCount)
+                    }
+                }
+            }
+        }
+        return obstructionPossibilitiesCount
     }
 
-
-
     val testInput = readInput("Day06_test")
-    part1(testInput).println()
+//    part1(testInput).println()
 //    part2(testInput).println()
 
     val input = readInput("Day06")
-    part1(input).println()
-//    part2(input).println()
+//    part1(input).println()
+    part2(input).println()
 
 }
