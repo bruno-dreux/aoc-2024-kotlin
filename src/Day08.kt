4acddd05ed14bcd228c8b2d978b7f1eb62d866f0
Bruno Dreux <bruno.dreux@Brunos-MacBook-Pro.local>
2024-12-20 18:54:11 -0300

    Solution to day 8, part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -10,21 +10,52 @@ fun main() {
     }
 
     fun getAntinodePositions(antenna1: Pair<Int,Int>, antenna2: Pair<Int,Int>): List<Pair<Int,Int>> {
-        val antionePositions: MutableList<Pair<Int,Int>> = mutableListOf()
+        val antinodePositions: MutableList<Pair<Int,Int>> = mutableListOf()
 
         val deltaRow = antenna2.first - antenna1.first
         val deltaColumn = antenna2.second - antenna1.second
 
-        antionePositions.add(Pair(antenna1.first - deltaRow,antenna1.second - deltaColumn))
-        antionePositions.add(Pair(antenna2.first + deltaRow,antenna2.second + deltaColumn))
+        antinodePositions.add(Pair(antenna1.first - deltaRow,antenna1.second - deltaColumn))
+        antinodePositions.add(Pair(antenna2.first + deltaRow,antenna2.second + deltaColumn))
 
-        return antionePositions
+        return antinodePositions
 
     }
 
     fun checkInBounds(map: MutableList<MutableList<Char>>, row: Int, column: Int): Boolean {
         return row >= 0 && row < map.size && column >= 0 && column < map[0].size
     }
+
+    fun getAntinodePositionsWithRepetition(map: MutableList<MutableList<Char>>,antenna1: Pair<Int,Int>, antenna2: Pair<Int,Int>): List<Pair<Int,Int>> {
+        val antinodePositions: MutableList<Pair<Int,Int>> = mutableListOf()
+
+        val deltaRow = antenna2.first - antenna1.first
+        val deltaColumn = antenna2.second - antenna1.second
+
+        var step: Int = 1
+
+        var inBounds: Boolean = checkInBounds(map,antenna1.first - deltaRow * step,antenna1.second - deltaColumn * step)
+
+        while(inBounds) {
+            antinodePositions.add(Pair(antenna1.first - deltaRow * step,antenna1.second - deltaColumn * step))
+            step++
+            inBounds = checkInBounds(map,antenna1.first - deltaRow * step,antenna1.second - deltaColumn * step)
+        }
+
+        step = 1
+        inBounds = checkInBounds(map,antenna2.first + deltaRow * step,antenna2.second + deltaColumn * step)
+
+        while(inBounds) {
+            antinodePositions.add(Pair(antenna2.first + deltaRow * step,antenna2.second + deltaColumn * step))
+            step++
+            inBounds = checkInBounds(map,antenna2.first + deltaRow * step,antenna2.second + deltaColumn * step)
+        }
+
+        return antinodePositions
+
+    }
+
+
 
     fun part1(input: List<String>): Int {
         val map: MutableList<MutableList<Char>> = input.map { it.toMutableList() }.toMutableList()
@@ -62,13 +93,49 @@ fun main() {
 
         printMap(mapOfAntinodes)
         return mapOfAntinodes.flatMap { it }.count { it == antinodeChar }
-
-        return 0
     }
 
 
     fun part2(input: List<String>): Int {
-        return 0
+        val map: MutableList<MutableList<Char>> = input.map { it.toMutableList() }.toMutableList()
+        val mapOfAntinodes: MutableList<MutableList<Char>> = input.map { it.toMutableList() }.toMutableList()
+        printMap(map)
+
+        for (row in map.indices) {
+            for (column in map[row].indices) {
+                if(map[row][column] != emptySpaceChar) { //current position is an antenna
+                    val currentAntenaChar = map[row][column]
+                    var listOfSimilarAntennas: MutableList<Pair<Int, Int>> = mutableListOf()
+                    println("Current antenna: $currentAntenaChar, at row: $row, column: $column")
+                    for (innerRow in row until map.size) {
+                        for (innerColumn in map[innerRow].indices) {
+                            if (map[innerRow][innerColumn] == currentAntenaChar) {
+                                listOfSimilarAntennas.add(Pair(innerRow, innerColumn))
+                            }
+                        }
+                    }
+                    if(listOfSimilarAntennas.size > 1) {
+                        for (similarAntenna in listOfSimilarAntennas) {
+                            mapOfAntinodes[similarAntenna.first][similarAntenna.second] = antinodeChar
+                        }
+
+                    }
+                    listOfSimilarAntennas = listOfSimilarAntennas.drop(1).toMutableList() //Gambiarra to remove the first element, as it is the current antenna
+                    println("Other positions with same antenna: $listOfSimilarAntennas")
+
+                    for (similarAntenna in listOfSimilarAntennas) {
+                        val potentialAntinodes = getAntinodePositionsWithRepetition(map,Pair(row,column), Pair(similarAntenna.first,similarAntenna.second))
+                        for (potentialAntinode in potentialAntinodes) {
+                            mapOfAntinodes[potentialAntinode.first][potentialAntinode.second] = antinodeChar
+                        }
+                    }
+
+                }
+            }
+        }
+
+        printMap(mapOfAntinodes)
+        return mapOfAntinodes.flatMap { it }.count { it == antinodeChar }
     }
 
 
@@ -80,6 +147,6 @@ fun main() {
 
     // Read the input from the `src/Day08.txt` file.
     val input = readInput("Day08")
-    part1(input).println()
-//    part2(input).println()
+//    part1(input).println()
+    part2(input).println()
 }
