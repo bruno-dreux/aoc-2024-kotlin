997c8be599363d5faf204a58492a8d4ff2d97d99
Bruno Dreux <bruno.dreux@Brunos-MacBook-Pro.local>
2024-12-25 21:15:39 +0100

    Solution to day 10, part 1

--- src/Day10.kt
+++ src/Day10.kt
@@ -4,34 +4,46 @@ fun main() {
         return row >=0 && row < map.size && column >=0 && column < map[row].size
     }
 
-    fun countSequencesFromPosition(map: List<List<Int>>, row: Int, column: Int): Int {
+    fun getEndingPoints(map: List<List<Int>>, row: Int, column: Int, endingPositions: MutableList<Pair<Int,Int>>) {
         val currentNumber = map[row][column]
-        var amountOfSequences = 0
 
-        if (currentNumber == 9) return 1
+        if (currentNumber == 9){
+            endingPositions.add(Pair(row, column))
+            return
+        }
 
         if(isInBounds(map, row -1, column) && map[row-1][column] == currentNumber+1) { //Position UP
-            amountOfSequences += countSequencesFromPosition(map, row-1, column)
+            getEndingPoints(map, row-1, column, endingPositions)
         }
 
         if(isInBounds(map, row, column-1) && map[row][column-1] == currentNumber+1) { //Position LEFT
-            amountOfSequences += countSequencesFromPosition(map, row, column-1)
+            getEndingPoints(map, row, column-1, endingPositions)
         }
 
         if(isInBounds(map, row, column+1) && map[row][column+1] == currentNumber+1) { //Position RIGHT
-            amountOfSequences += countSequencesFromPosition(map, row, column+1)
+            getEndingPoints(map, row, column+1, endingPositions)
         }
 
         if(isInBounds(map, row +1, column) && map[row+1][column] == currentNumber+1) { //Position DOWN
-            amountOfSequences += countSequencesFromPosition(map, row+1, column)
+            getEndingPoints(map, row+1, column, endingPositions)
         }
 
-        return amountOfSequences
+        return
     }
 
     fun countSequencesFromZero(map: List<List<Int>>, row: Int, column: Int): Int {
         if (map[row][column] == 0) {
-            return countSequencesFromPosition(map, row, column)
+            var listOfEndingPointsPositions: MutableList<Pair<Int,Int>> = mutableListOf()
+            getEndingPoints(map, row, column, listOfEndingPointsPositions)
+
+            var deduplicatedPositions: MutableList<Pair<Int,Int>> = mutableListOf()
+
+            for (position in listOfEndingPointsPositions) {
+                if (!(position in deduplicatedPositions)) {
+                    deduplicatedPositions.add(position)
+                }
+            }
+            return deduplicatedPositions.count()
         }
         return 0
     }
@@ -51,10 +63,15 @@ fun main() {
     fun part1(input: List<String>): Int {
         val map = readMap(input)
 
-        println(countSequencesFromZero(map, 0,0))
-        //TO DO: understand why the number is too high and fix it
+        var sumOfTrailheads: Int = 0
 
-        return 0
+        for (row in map.indices) {
+            for (column in map[row].indices) {
+                sumOfTrailheads += countSequencesFromZero(map,row,column)
+            }
+        }
+
+        return sumOfTrailheads
     }
 
 
@@ -65,12 +82,12 @@ fun main() {
 
 
     // Or read a large test input from the file:
-    val testInput = readInput("Day10_test")
-    part1(testInput).println()
+//    val testInput = readInput("Day10_test")
+//    part1(testInput).println()
 //    part2(testInput).println()
 
     // Read the input from the file.
-//    val input = readInput("Day10")
-//    part1(input).println()
+    val input = readInput("Day10")
+    part1(input).println()
 //    part2(input).println()
 }
