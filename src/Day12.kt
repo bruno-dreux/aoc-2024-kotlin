7c2e63fcfc0326763835c072ce4671f7c574dc3e
Bruno Dreux <bruno.dreux@Brunos-MacBook-Pro.local>
2024-12-29 19:01:50 +0100

    A lot of code for an attempt at day 12, part 2, but not finished

--- src/Day12.kt
+++ src/Day12.kt
@@ -105,13 +105,74 @@ fun main() {
         return listPrices.sum()
     }
 
+    fun calculateSides(setSimilarElements: MutableSet<Pair<Int,Int>>): Int {
+        if (setSimilarElements.isEmpty()) return 0
+        if (setSimilarElements.size == 1 || setSimilarElements.size == 2) return 4
+        var sides: Int = 4
+        for (currentElement in setSimilarElements) {
+            var isNewSide = 1
+            for (otherElement in setSimilarElements) {
+                if (abs(otherElement.first-currentElement.first) == 0 && abs(otherElement.second-currentElement.second) == 1) {
+                    isNewSide = 0
+                }
+                else if (abs(otherElement.first-currentElement.first) == 1 && abs(otherElement.second-currentElement.second) == 0) {
+                    isNewSide = 0
+                }
+            }
+            sides += isNewSide
+        }
+        return sides
+    }
+
+    fun countCornersForPosition(map: MutableList<MutableList<Char>>,row: Int, column: Int): Int {
+        val currentValue = map[row][column]
+        val neighbors: MutableMap<String,Int> = mutableMapOf()
+        neighbors.put("left",0)
+        neighbors.put("right",0)
+        neighbors.put("up",0)
+        neighbors.put("down",0)
+        neighbors.put("topleft",0)
+        neighbors.put("topright",0)
+        neighbors.put("bottomleft",0)
+        neighbors.put("bottomright",0)
+        if(isInBounds(map,row,column-1) && map[row][column-1] == currentValue) neighbors.put("left",1)
+        if(isInBounds(map,row,column+1) && map[row][column+1] == currentValue) neighbors.put("right",1)
+        if(isInBounds(map,row-1,column) && map[row-1][column] == currentValue) neighbors.put("up",1)
+        if(isInBounds(map,row+1,column) && map[row+1][column] == currentValue) neighbors.put("down",1)
+        if(isInBounds(map,row-1,column-1) && map[row-1][column-1] == currentValue) neighbors.put("topleft",1)
+        if(isInBounds(map,row-1,column+1) && map[row-1][column+1] == currentValue) neighbors.put("topright",1)
+        if(isInBounds(map,row+1,column-1) && map[row+1][column-1] == currentValue) neighbors.put("bottomleft",1)
+        if(isInBounds(map,row+1,column+1) && map[row+1][column+1] == currentValue) neighbors.put("bottomright",1)
+
+        if(neighbors["left"] == 0 && neighbors["right"] == 0 && neighbors["up"] == 0 && neighbors["down"] == 0) { //No neighbors, 4 sides
+            return 4
+        }
+        else if (neighbors.values.sum() == 1) { //One of the four side corners is 1
+            return 4
+        }
+        else if (neighbors.values.sum() == 8) {
+            return 0
+        }
+        else if (neighbors["left"] == 1 && neighbors["right"] == 1 && neighbors["up"] == 0 && neighbors["down"] == 0) { //Opposing corners case 1
+            return 0
+        }
+        else if (neighbors["left"] == 0 && neighbors["right"] == 0 && neighbors["up"] == 1 && neighbors["down"] == 1) { //Opposing corners case 2
+            return 0
+        }
+        //TODO: need to continue with other cases
+        else{ //This should never be reached
+            return -1
+        }
+
+    }
+
 
     fun part2(input: List<String>): Int {
         val map = processInput(input)
         println(map)
 
         val mapReached: MutableList<MutableList<Int>> = mutableListOf()  //Map with the positions already reached
-        val listPerimeters: MutableList<Int> = mutableListOf()
+        val listSides: MutableList<Int> = mutableListOf()
         val listAreas: MutableList<Int> = mutableListOf()
         val listPrices: MutableList<Int> = mutableListOf()
 
@@ -130,18 +191,19 @@ fun main() {
                     setSimilarElements = findSimilarElements(map,setSimilarElements,map[row][column],row,column)
                     println("${map[row][column]}: $setSimilarElements")
 
-                    var similarElementsSides: Int = 4
-                    //TODO: Calculate the similar elements sides from the set
+                    val similarElementsSides = calculateSides(setSimilarElements)
 
                     for (element in setSimilarElements) {
                         mapReached[element.first][element.second] = 1
                     }
                     listAreas.add(setSimilarElements.size)
-                    listPerimeters.add(similarElementsSides)
+                    listSides.add(similarElementsSides)
                     listPrices.add(setSimilarElements.size*similarElementsSides)
                 }
             }
         }
+
+        println(listSides)
 
         return listPrices.sum()
     }
