428d1c98d9055960740051edaacc4647bb042a99
Bruno Dreux <bruno.dreux@Brunos-MacBook-Pro.local>
2024-12-31 14:53:07 +0100

    Solution to Day 13, part 2

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,12 +1,13 @@
 import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 fun main() {
 
-    class linearSystem(val ax: Int,val  bx: Int,val resultx: Int,val ay: Int,val by: Int,val resulty: Int) {
+    class linearSystem(val ax: Long,val  bx: Long,var resultx: Long,val ay: Long,val by: Long,var resulty: Long) {
         var a: Double = -1.0
         var b: Double = -1.0
-        var aInt: Int = -1
-        var bInt: Int = -1
+        var aInt: Long = -1
+        var bInt: Long = -1
 
         override fun toString(): String {
             return "Linear system: $ax, $bx, $resultx, $ay, $by, $resulty"
@@ -18,8 +19,8 @@ fun main() {
             if(denominator != 0.0) {
                 b = numerator/denominator
                 a = (resultx-bx*b)/ax
-                aInt = a.roundToInt()
-                bInt = b.roundToInt()
+                aInt = a.roundToLong()
+                bInt = b.roundToLong()
             }
             else{
                 println("Denominator is 0")
@@ -31,6 +32,11 @@ fun main() {
         fun checkIntegerSolution(): Boolean{
             return ax*aInt+bx*bInt == resultx && ay*aInt+by*bInt == resulty
         }
+
+        fun adjustInput() {
+            resultx += 10000000000000
+            resulty += 10000000000000
+        }
     }
 
 
@@ -38,28 +44,28 @@ fun main() {
         val regexAB = Regex("""X\+(\d+), Y\+(\d+)""")
         val regexResult = Regex("""X=(\d+), Y=(\d+)""")
         val output = mutableListOf<linearSystem>()
-        var ax: Int = 0
-        var ay: Int = 0
-        var bx: Int = 0
-        var by: Int = 0
-        var resultx: Int = 0
-        var resulty: Int = 0
+        var ax: Long = 0
+        var ay: Long = 0
+        var bx: Long = 0
+        var by: Long = 0
+        var resultx: Long = 0
+        var resulty: Long = 0
         for (row in input.indices) {
             if(input[row].contains("Button A:")) {
                 val matchResultA = regexAB.find(input[row])
                 if (matchResultA != null) {
-                    ax = matchResultA.groupValues[1].toInt()
-                    ay = matchResultA.groupValues[2].toInt()
+                    ax = matchResultA.groupValues[1].toLong()
+                    ay = matchResultA.groupValues[2].toLong()
                 }
                 val matchResultB = regexAB.find(input[row+1])
                 if (matchResultB != null) {
-                    bx = matchResultB.groupValues[1].toInt()
-                    by = matchResultB.groupValues[2].toInt()
+                    bx = matchResultB.groupValues[1].toLong()
+                    by = matchResultB.groupValues[2].toLong()
                 }
                 val matchResultResult = regexResult.find(input[row+2])
                 if (matchResultResult != null) {
-                    resultx = matchResultResult.groupValues[1].toInt()
-                    resulty = matchResultResult.groupValues[2].toInt()
+                    resultx = matchResultResult.groupValues[1].toLong()
+                    resulty = matchResultResult.groupValues[2].toLong()
                 }
                 output.add(linearSystem(ax,bx,resultx,ay,by,resulty))
             }
@@ -68,9 +74,9 @@ fun main() {
     }
 
 
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<String>): Long {
         var listSystems = processInput(input)
-        var tokensSum = 0
+        var tokensSum: Long = 0
 
         for (system in listSystems) {
             println(system)
@@ -85,8 +91,21 @@ fun main() {
     }
 
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        var listSystems = processInput(input)
+        var tokensSum: Long = 0
+
+        for (system in listSystems) {
+            system.adjustInput()
+            println(system)
+            system.solve()
+            println("A = ${system.a}/${system.aInt}, B = ${system.b}/${system.bInt}")
+            if(system.checkIntegerSolution()) {
+                println("Valid solution to system, A: ${system.aInt}, B: ${system.bInt}")
+                tokensSum += system.aInt*3 + system.bInt
+            }
+        }
+        return tokensSum
     }
 
 
@@ -98,6 +117,6 @@ fun main() {
 
     // Read the input from the file.
     val input = readInput("Day13")
-    part1(input).println()
-//    part2(input).println()
+//    part1(input).println()
+    part2(input).println()
 }
